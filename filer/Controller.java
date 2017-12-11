16a33f6af27e7ca7ab4d80649298efb92ed97d63
Luke <lukewaggy@googlemail.com>
2017-12-10 21:26:21 -0400

    Debug 'Clear' Button

--- filer/Controller.java
+++ filer/Controller.java
@@ -1,7 +1,5 @@
 package filer;
 
-//testing git
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
--- filer/Main.java
+++ filer/Main.java
@@ -1,7 +1,5 @@
 package filer;
 
-//git test
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
