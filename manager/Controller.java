a673989a2cf3ce619fe027e4ec153d3d79bbdb2f
Luke <lukewaggy@googlemail.com>
2017-12-15 19:52:28 -0400

    Add drag and drop/file choosing skeleton

--- manager/Controller.java
+++ manager/Controller.java
@@ -4,18 +4,26 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
 
 public class Controller {
 
@@ -28,7 +36,10 @@ public class Controller {
 
     );
 
-    private final ArrayList<String> imgFileTypes = new ArrayList<>();
+    private ArrayList<String> imgFileTypes = new ArrayList<>();
+
+    private ArrayList<File> fileQueue = new ArrayList<>();
+    private ArrayList<File> pictureQueue = new ArrayList<>();
 
     @FXML
     private TextField firstName;
@@ -48,50 +59,72 @@ public class Controller {
     @FXML
     private TextField status;
 
+    private final File rootDir = new File("E:/Computer Science/Files");
+
     final private DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM.dd.yyyy");
 
-    public void exit(ActionEvent actionEvent) {
-
-        System.exit(0);
-
-    }
-
     @FXML
-    private void createFilesystem(ActionEvent actionEvent) throws IOException {
+    void handleCreateButton() throws IOException {
 
-        if(lastName.getText() == null || dateOfLoss.getValue() == null || aagNumber.getText() == null) {
+        if(Objects.equals(lastName.getText(), "") || dateOfLoss.getValue() == null || Objects.equals(aagNumber.getText(), "")) {
 
-            status.setText("Please fill in the required fields denotated by '*'.");
+            status.setText("Please fill in the required fields denoted by '*'.");
 
         } else {
 
-            final File rootDir = new File("E:/Computer Science/Files");
+            if(fileQueue.size() == 0 && pictureQueue.size() == 0) {
 
-            if (rootDir.exists() && rootDir.isDirectory()) {
+                Alert noFilesAlert = new Alert(Alert.AlertType.CONFIRMATION, "No files or pictures have been added. Is " +
+                        "this correct? You will have to add them manually later.", ButtonType.OK, ButtonType.CANCEL);
+                noFilesAlert.showAndWait();
 
-                status.setText("Root directory located, creating filesystem...");
-                createClientDir(rootDir);
+                if(noFilesAlert.getResult() == ButtonType.OK) {
 
-            } else if(!rootDir.canWrite()){
+                    createFilesystem();
+                    moveFiles(fileQueue, pictureQueue);
 
-                status.setText("Root directory could not be accessed. Contact Admin.");
+                } else if(noFilesAlert.getResult() == ButtonType.CANCEL) {
+
+
+
+                }
 
             } else {
 
-                status.setText("Something went wrong. Contact Admin.");
-
-            }
+                createFilesystem();
+                moveFiles(fileQueue, pictureQueue);
 
             }
 
         }
 
-    private void createClientDir(File root) {
+    }
+
+    void createFilesystem() throws IOException {
+
+        if (rootDir.exists() && rootDir.isDirectory()) {
+
+            status.setText("Root directory located, creating filesystem...");
+            createClientDir(rootDir);
+
+        } else if(!rootDir.canWrite()){
+
+            status.setText("Root directory could not be accessed. Contact Admin.");
+
+        } else {
+
+            status.setText("Something went wrong. Contact Admin.");
+
+        }
+
+    }
+
+    void createClientDir(File root) {
 
         String formattedDate = dateFormat.format(dateOfLoss.getValue());
 
         File claimDir = new File(root.getPath() + "/" +
-                lastName.getText() + " " + formattedDate + " AAG " + aagNumber.getText());
+                lastName.getText() + " " + firstName.getText() + " " + formattedDate + " AAG " + aagNumber.getText());
 
         try {
 
@@ -104,15 +137,21 @@ public class Controller {
 
         }
 
+        flushQueue();
+
     }
 
-    public void populateDamageTypes(MouseEvent mouseEvent) {
+    @FXML
+    void populateDamageTypes(MouseEvent mouseEvent) {
 
         damageType.setItems(damageTypes);
 
     }
 
-    public void clearForm(ActionEvent actionEvent) {
+    @FXML
+    void clearForm(ActionEvent actionEvent) {
+        System.out.println(pictureQueue);
+
 
         firstName.setText(null);
         lastName.setText(null);
@@ -120,23 +159,120 @@ public class Controller {
         damageType.setValue(null);
         aagNumber.setText(null);
         status.setText(null);
+        flushQueue();
 
     }
 
-    public void handleDragDrop(DragEvent dragEvent) {
+    @FXML
+    void chooseFiles() {
+
+        FileChooser fileChooser = new FileChooser();
+
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(new Stage());
+
+        if(selectedFiles != null) {
+
+            sortFiles(selectedFiles);
+
+        }
+
+
+    }
+
+    @FXML
+    void handleDragOver(DragEvent dragEvent) {
+
+        if(dragEvent.getDragboard().hasFiles()) {
+
+            dragEvent.acceptTransferModes(TransferMode.MOVE);
+
+        }
+
+        dragEvent.consume();
+
+    }
+
+    @FXML
+    void handleDragDrop(DragEvent dragEvent) throws FileNotFoundException {
 
         Dragboard dragboard = dragEvent.getDragboard();
+        List<File> droppedFiles = dragboard.getFiles();
+
         if(dragboard.hasFiles()) {
-            sortFiles(dragboard.getFiles());
+
+            sortFiles(droppedFiles);
+            System.out.print(pictureQueue + "\n" + fileQueue);
+
         }
 
     }
 
-    void sortFiles(List<File> files) {
+    private void sortFiles(List<File> files) {
+
+        imgFileTypes.add(".png");
+        imgFileTypes.add(".jpeg");
+        imgFileTypes.add(".gif");
+        imgFileTypes.add(".tiff");
 
         for(File file : files) {
-            // sort code here
+
+            if( file != null) {
+
+                String fileName = file.getName();
+                String fileExt = fileName.substring(fileName.lastIndexOf("."));
+
+
+                if(imgFileTypes.contains(fileExt)) {
+
+                    pictureQueue.add(file);
+
+                } else {
+
+                    fileQueue.add(file);
+
+                }
+
+            }
+
         }
+    }
+
+    public void moveFiles(ArrayList<File> files, ArrayList<File> pictures) throws IOException {
+
+        for(File file : files) {
+
+            Files.move(Paths.get(file.getPath()), Paths.get(new File(rootDir + "/").getPath()), StandardCopyOption.REPLACE_EXISTING);
+
+        }
+
+        if(!pictureQueue.isEmpty()) {
+
+            File pictureDir = new File(rootDir + "/Pictures");
+
+            for(File file : pictures) {
+
+                Files.move(Paths.get(file.getPath()), Paths.get(new File(rootDir + "/Pictures").getPath()), StandardCopyOption.REPLACE_EXISTING);
+
+            }
+
+        }
+
+
+
+    }
+
+    @FXML
+    void exit(ActionEvent actionEvent) {
+
+        System.exit(0);
+
+    }
+
+    void flushQueue() {
+
+        fileQueue.clear();
+        pictureQueue.clear();
+
     }
 
 }
